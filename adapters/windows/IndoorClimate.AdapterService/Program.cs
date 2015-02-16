65fa26b42cf26e3c065f5d6ee88b3ca5e178ae00
Vladislav Hloboschin <hloboshin.vladislav@gmail.com>
2015-02-16 19:28:48 +0300

    added configurable interval

--- adapters/windows/IndoorClimate.AdapterService/Program.cs
+++ adapters/windows/IndoorClimate.AdapterService/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 using Quartz;
 using Topshelf;
 using Topshelf.Quartz;
@@ -11,6 +7,11 @@ namespace IndoorClimate.AdapterService
 {
     internal class Program
     {
+        private static int Interval
+        {
+            get { return int.Parse(ConfigurationManager.AppSettings["Interval"]); }
+        }
+
         private static void Main(string[] args)
         {
             HostFactory.Run(x =>
@@ -25,7 +26,7 @@ namespace IndoorClimate.AdapterService
                     .AddTrigger(() =>
                         TriggerBuilder.Create()
                             .WithSimpleSchedule(builder => builder
-                                .WithIntervalInSeconds(5)
+                                .WithIntervalInSeconds(Interval)
                                 .RepeatForever())
                             .Build())
                     );
