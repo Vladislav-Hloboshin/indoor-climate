20bd6479759e498c7fdcccd99a1b92680f6e454a
Vladislav Hloboschin <hloboshin.vladislav@gmail.com>
2015-03-16 17:52:51 +0300

    added index annotation to files code and date

--- server/src/main/java/org/vladhd/indoorclimate/domain/ClimateData.java
+++ server/src/main/java/org/vladhd/indoorclimate/domain/ClimateData.java
@@ -2,6 +2,7 @@ package org.vladhd.indoorclimate.domain;
 
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
 import org.joda.time.DateTime;
 
 import java.io.Serializable;
@@ -14,8 +15,10 @@ public class ClimateData implements Serializable {
     @Id
     public Long id;
 
+    @Index
     public String code;
 
+    @Index
     public DateTime date;
 
     int co2;
