d0a3b07faddd162dff56b787115245edc6e7f9a3
Vladislav-Hloboshin <474b894585>
2015-03-26 01:35:26 +0300

    added packed data save

--- server/src/main/java/org/vladhd/indoorclimate/DataServlet.java
+++ server/src/main/java/org/vladhd/indoorclimate/DataServlet.java
@@ -1,6 +1,7 @@
 package org.vladhd.indoorclimate;
 
 import com.fatboyindustrial.gsonjodatime.Converters;
+import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -15,20 +16,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import static com.googlecode.objectify.ObjectifyService.factory;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 public class DataServlet extends HttpServlet {
 
+    private static int saveDeltaInMinutes = 5;
+
     public void init(){
         JodaTimeTranslators.add(factory());
-        factory().register(ClimateData.class);
+        factory().register(PackedClimateData.class);
     }
 
     @Override
     public void doGet(HttpServletRequest req, final HttpServletResponse resp)
             throws IOException {
+
+        MemcacheService memcacheService = MemcacheServiceFactory.getMemcacheService();
+
         resp.setContentType("text/plain");
         final Gson gson = Converters.registerDateTime(new GsonBuilder()).create();
 
@@ -41,15 +48,20 @@ public class DataServlet extends HttpServlet {
         Object response = null;
         switch(method){
             case "actual":
-                response = MemcacheServiceFactory.getMemcacheService().get("LastClimateData_" + code);
+                response = memcacheService.get("LastClimateData_" + code);
                 break;
             case "last":
-                response = ofy().load()
+                List<PackedClimateData> dataList = ofy().load()
                         .type(PackedClimateData.class)
                         .filter("code", code)
                         .order("-date")
-                        .limit(100)
+                        .limit(288)//one day (saveDeltaInMinutes = 5)
                         .list();
+                PackedClimateData packedClimateData = (PackedClimateData)memcacheService.get("LastPackedClimateData_" + code);
+                if(packedClimateData!=null){
+                    dataList.add(0, packedClimateData);
+                }
+                response = dataList;
                 break;
             case "history":
                 DateTime from = DateTime.parse(req.getParameter("from"));
@@ -60,7 +72,7 @@ public class DataServlet extends HttpServlet {
                         .filter("date >", from)
                         .filter("date <", to)
                         .order("-date")
-                        .limit(300)
+                        .limit(576)//two days (saveDeltaInMinutes = 5)
                         .list();
                 break;
             }
@@ -71,24 +83,24 @@ public class DataServlet extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException{
 
+        MemcacheService memcacheService = MemcacheServiceFactory.getMemcacheService();
+
         String code = req.getParameter("code");
         int co2 = Integer.parseInt(req.getParameter("co2"));
         double temp = Double.parseDouble(req.getParameter("temp"));
 
         DateTime now = DateTime.now(DateTimeZone.UTC);
-        DateTime roundedNow = now.minuteOfHour().roundFloorCopy().minusMinutes(now.getMinuteOfHour() % 5);
+        DateTime roundedNow = now.minuteOfHour().roundFloorCopy().minusMinutes(now.getMinuteOfHour() % saveDeltaInMinutes);
 
         ClimateData climateData = new ClimateData(now, co2, temp, 0);
 
-        MemcacheServiceFactory.getMemcacheService().put("LastClimateData_" + code, climateData);
+        memcacheService.put("LastClimateData_" + code, climateData);
 
-        PackedClimateData packedClimateData = (PackedClimateData)MemcacheServiceFactory
-                .getMemcacheService()
-                .get("LastPackedClimateData_" + code);
+        PackedClimateData packedClimateData = (PackedClimateData)memcacheService.get("LastPackedClimateData_" + code);
 
         if(packedClimateData==null){
             packedClimateData = new PackedClimateData(code, roundedNow);
-        } else if(packedClimateData.date == null){
+        } else if(packedClimateData.date.equals(roundedNow)==false){
             ofy().save().entity(packedClimateData).now();
             packedClimateData = new PackedClimateData(code, roundedNow);
         }
